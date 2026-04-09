de0fdcd50839b948a1c451876e958fbff2e4b070
Hamt3er <hydenarcade@gmail.com>
2026-04-09 15:43:54 +0000

    Clean repo: remove binaries and test files

--- test.js
+++
@@ -1,5 +0,0 @@
-const api_key = "AIzaSyEXAMPLEEXAMPLEEXAMPLE12345";
-const token = "ghp_exampletoken12345678901234567890";
-fetch("/api/user/profile");
-localStorage.setItem("authToken", "123");
-eval("console.log('test')");
